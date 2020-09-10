f67f6e51540a5e66eb650514e5cbc702a48813f9
Joy-IT <service@joy-it.net>
2020-09-10 15:07:43 +0200

    Update PCF8591.py
    English comments

--- PCF8591/PCF8591.py
+++ PCF8591/PCF8591.py
@@ -2,20 +2,20 @@
 import smbus
 from time import sleep
 
-i2c = smbus.SMBus(1)     # Erzeugen und oeffnen einer I2C-Instanz
+i2c = smbus.SMBus(1)     
 
 def readADCvalue(i2cDeviceAddr, channel):
    value=i2c.read_byte_data (i2cDeviceAddr, channel)  
    sleep (0.01)
-   value=i2c.read_byte_data (i2cDeviceAddr, channel)  # aktueller Wert wird ausgelesen
+   value=i2c.read_byte_data (i2cDeviceAddr, channel)  # read current analog value
    return value
 
 def readADCvoltage(i2cDeviceAddr, channel):
    value=i2c.read_byte_data (i2cDeviceAddr, channel)  
    sleep (0.01)
-   value=i2c.read_byte_data (i2cDeviceAddr, channel)  # aktueller Wert wird ausgelesen
-   value= value * (3.3 / 255)                         # Umrechnung in eine Spannung
+   value=i2c.read_byte_data (i2cDeviceAddr, channel)  # read current analog value
+   value= value * (3.3 / 255)                         # conversion to a voltage
    return value
 
 def writeADC(i2cDeviceAddr, channel, value):
-   i2c.write_byte_data (i2cDeviceAddr, 0x40+channel, value) 
+   i2c.write_byte_data (i2cDeviceAddr, 0x40+channel, value) # write a value to the ADC output
