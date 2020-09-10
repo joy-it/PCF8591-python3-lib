56aba57c96241d216a1aca0dbf7346cba4d1b4c4
Joy-IT <service@joy-it.net>
2020-09-10 15:10:54 +0200

    Update write.py

--- examples/write.py
+++ examples/write.py
@@ -2,7 +2,7 @@ import PCF8591
 from time import sleep
 
 adc = PCF8591
-i2cAddress = 0x48          # I2C-Adresse des ADCs
+i2cAddress = 0x48          # ADC I²C-Address
 CH0 = 0x00  # Channel 0
 CH1 = 0x01  # Channel 1
 CH2 = 0x02  # Channel 2
