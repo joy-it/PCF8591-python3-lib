cf04c73dfd40e6d1f0e6598917940ae193056f7f
Joy-IT <service@joy-it.net>
2020-09-17 09:49:27 +0200

    Update PCF8591.py

--- PCF8591/PCF8591.py
+++ PCF8591/PCF8591.py
@@ -5,8 +5,6 @@ from time import sleep
 i2c = smbus.SMBus(1)     
 
 def readADCvalue(i2cDeviceAddr, channel):
-   value=i2c.read_byte_data (i2cDeviceAddr, channel)  
-   sleep (0.01)
    value=i2c.read_byte_data (i2cDeviceAddr, channel)  # read current analog value
    return value
 
